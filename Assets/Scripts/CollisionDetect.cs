a75a9b681406d014ff49af8b693f24123818c1c7
Allan <allanjaybusante3@gmail.com>
2026-03-02 09:17:49 +0800

    feat: add a collision and fade out effect

--- Assets/Scripts/CollisionDetect.cs
+++ Assets/Scripts/CollisionDetect.cs
@@ -1,12 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionDetect : MonoBehaviour
 {
     [SerializeField] GameObject thePlayer;
     [SerializeField] GameObject playerAnimation;
+    [SerializeField] AudioSource collisionFX;
+    [SerializeField] GameObject mainCam;
+    [SerializeField] GameObject fadeOut;
     void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(CollisionEnd());
+    }
+
+    IEnumerator CollisionEnd()
+    {
+        collisionFX.Play();
         thePlayer.GetComponent<PlayerMovement>().enabled = false;
         playerAnimation.GetComponent<Animator>().Play("Stumble Backwards");
+        mainCam.GetComponent<Animator>().Play("CollisionCam");
+        yield return new WaitForSeconds(2);
+        fadeOut.SetActive(true);
     }
 }
