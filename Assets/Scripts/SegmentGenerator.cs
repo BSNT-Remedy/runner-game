ad74d291a67f9046df6882142fbf7e10be14224c
allan <allanjaybusante3@gmail.com>
2026-03-04 20:09:48 +0800

    feat: extend the visible environment distance  and add fog effect

--- Assets/Scripts/SegmentGenerator.cs
+++ Assets/Scripts/SegmentGenerator.cs
@@ -6,33 +6,14 @@ public class SegmentGenerator : MonoBehaviour
 {
     public GameObject[] segment;
     public float segmentSpeed = 6;
-    [SerializeField] int zPos = 100;
-    // [SerializeField] bool creatingSegment = false;
+    // [SerializeField] int zPos = 100;
     [SerializeField] int segmentNum;
-    // void Update()
-    // {
-    //     if(creatingSegment == false)
-    //     {
-    //         creatingSegment = true;
-    //         StartCoroutine(SegmentGen());
-    //     }
-    // }
 
-    // IEnumerator SegmentGen() {
-    //     segmentNum = Random.Range(0, 3);
-    //     GameObject newSeg = Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
-    //     newSeg.SetActive(true);
-    //     // zPos += 50;
-    //     yield return new WaitForSeconds(3);
-    //     creatingSegment = false;
-    // }
-
-    
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Trigger")) {
             segmentNum = Random.Range(0, 3);
-            GameObject newSeg = Instantiate(segment[segmentNum], new Vector3(0, 0, zPos), Quaternion.identity);
+            GameObject newSeg = Instantiate(segment[segmentNum], new Vector3(0, 0, 100), Quaternion.identity);
             newSeg.SetActive(true);
         }
     }
