d7b6faf61790fa3ee8473c8ba75c882763f1ba09
allan <allanjaybusante3@gmail.com>
2026-03-25 19:35:58 +0800

    feat: add camera transitions for smooth jump and slide animation

--- Assets/Scripts/SwipeJumpSlideController.cs
+++ Assets/Scripts/SwipeJumpSlideController.cs
@@ -1,7 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SwipeJumpSlideController : MonoBehaviour
 {
+    [SerializeField] GameObject cam;
+    [SerializeField] GameObject subCam;
     
     [Header("Swipe Settings")]
     public float minSwipeDistance = 50f;
@@ -9,29 +13,39 @@ public class SwipeJumpSlideController : MonoBehaviour
     private bool isSwiping = false;
 
     [Header("Jump Settings (No Physics)")]
-    public float jumpHeight = 5f;
-    public float jumpDuration = 0.5f;
+    public float jumpHeight = 3f;
+    public float jumpDuration = 0.35f;
     private bool isJumping = false;
-
+    
     [Header("Slide Settings")]
-    public float slideDuration = 0.7f;
+    public float slideDuration = 0.35f;
     private bool isSliding = false;
 
     private CapsuleCollider col;
     private float originalHeight;
     private Vector3 originalPosition;
 
+    Vector3 freezeCamPos;
+    Quaternion freezeCamRot;
+    private Vector3 subCamInitialLocalPos;
+    private Quaternion subCamInitialLocalRot;
+
     void Start()
     {
         col = GetComponent<CapsuleCollider>();
         originalHeight = col.height;
         originalPosition = transform.localPosition;
+ 
+        subCamInitialLocalPos = subCam.transform.localPosition;
+        subCamInitialLocalRot = subCam.transform.localRotation;
     }
 
     void Update()
     {
         DetectSwipe();
     }
+
+    
 
     // -------------------------------------------------
     // SWIPE DETECTION (EDITOR + MOBILE)
@@ -93,7 +107,7 @@ public class SwipeJumpSlideController : MonoBehaviour
     // -------------------------------------------------
     void Jump()
     {
-        if (isJumping || isSliding) return;
+        if (isJumping) return;
 
         StartCoroutine(JumpRoutine());
     }
@@ -101,37 +115,55 @@ public class SwipeJumpSlideController : MonoBehaviour
     System.Collections.IEnumerator JumpRoutine()
     {
         isJumping = true;
+        FreezeCamera();
 
+        float half = jumpDuration / 2f;
         float timer = 0f;
-        Vector3 startPos = new Vector3(transform.position.x, 2, transform.position.z);
-        Vector3 peakPos = new Vector3(transform.position.x, jumpHeight, transform.position.z);
+
+        float baseY = transform.localPosition.y;
+        float peakY = baseY + jumpHeight;
+
+        Vector3 pos;
 
         // UPWARD PHASE
-        while (timer < jumpDuration / 2f)
+        while (timer < half)
         {
             timer += Time.deltaTime;
-            float t = timer / (jumpDuration / 2f);
-            transform.localPosition = Vector3.Lerp(startPos, peakPos, t);
+            float t = timer / half;
+
+            pos = transform.localPosition;
+            pos.y = Mathf.Lerp(baseY, peakY, t);
+            transform.localPosition = pos;
+
             yield return null;
         }
 
         // DOWNWARD PHASE
         timer = 0f;
-        while (timer < jumpDuration / 2f)
+        while (timer < half)
         {
             timer += Time.deltaTime;
-            float t = timer / (jumpDuration / 2f);
-            transform.localPosition = Vector3.Lerp(peakPos, startPos, t);
+            float t = timer / half;
+
+            pos = transform.localPosition;
+            pos.y = Mathf.Lerp(peakY, baseY, t);
+            transform.localPosition = pos;
+
             yield return null;
         }
 
+        if(!isSliding){
+            UnFreezeCamera();
+        }
+        
         isJumping = false;
     }
 
 
+
     void Slide()
     {
-        if (isSliding || isJumping) return;
+        if (isSliding) return;
 
         StartCoroutine(SlideRoutine());
     }
@@ -139,6 +171,7 @@ public class SwipeJumpSlideController : MonoBehaviour
     System.Collections.IEnumerator SlideRoutine()
     {
         isSliding = true;
+        FreezeCamera();
 
         // --- ROTATE TO -90 ---
         float duration = 0.2f; // how fast to tilt down
@@ -181,7 +214,37 @@ public class SwipeJumpSlideController : MonoBehaviour
             yield return null;
         }
 
+        if(!isJumping){
+            UnFreezeCamera();
+        }
+
         isSliding = false;
+    }
+
+    // void FreezeCamera()
+    // {
+    //     subCam.transform.SetParent(null, true);
+    //     subCam.SetActive(true);
+    //     cam.SetActive(false);
+    // }
+
+    // void UnFreezeCamera() {        
+    //     subCam.transform.localPosition = subCamInitialLocalPos;
+    //     subCam.transform.localRotation  = subCamInitialLocalRot;
+
+    //     subCam.transform.SetParent(this.transform, false);
+    //     cam.SetActive(true);
+    //     subCam.SetActive(false);
+    // }
+
+
+    void FreezeCamera()
+    {
+        cam.SetActive(false);
+    }
+
+    void UnFreezeCamera() {        
+        cam.SetActive(true);
     }
 
 }
